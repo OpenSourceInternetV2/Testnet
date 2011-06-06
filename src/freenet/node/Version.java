5b0c2046dd1eb1211d06ebe8dbd63f7ef1abf513
Matthew Toseland <toad@amphibian.dyndns.org>
2011-06-06 21:35:07 +0100

    Testnet build 54. Didn't commit this in tag. Scripts fixed to avoid in future.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 53;
+	private static final int buildNumber = 54;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 52;
-	private static final int newLastGoodBuild = 53;
+	private static final int oldLastGoodBuild = 53;
+	private static final int newLastGoodBuild = 54;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.MAY, 28, 0, 0, 0 );
+		_cal.set( 2011, Calendar.JUNE, 2, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
