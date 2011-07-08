37fcd79fcc7e1807e42fd10e1c392e0d80aab756
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-08 18:37:04 +0100

    59. Mandatory midnight.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 58;
+	private static final int buildNumber = 59;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 57;
-	private static final int newLastGoodBuild = 58;
+	private static final int newLastGoodBuild = 59;
 	static final long transitionTime;
 
 	static {
