1dca51f24917ceb9c4a2c0e60209b7c8f397327b
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-24 00:51:59 +0100

    Testnet build 69. Includes important fixes for inserts on NLM.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 67;
+	private static final int buildNumber = 69;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 66;
-	private static final int newLastGoodBuild = 67;
+	private static final int newLastGoodBuild = 69;
 	static final long transitionTime;
 
 	static {
