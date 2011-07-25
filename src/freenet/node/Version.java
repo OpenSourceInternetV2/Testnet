e4c086e7350861c7b8d4e571c83bd098d13b5805
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-25 15:12:21 +0100

    Testnet build 70. Mandatory tomorrow.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 69;
+	private static final int buildNumber = 70;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 66;
-	private static final int newLastGoodBuild = 69;
+	private static final int oldLastGoodBuild = 69;
+	private static final int newLastGoodBuild = 70;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.JULY, 24, 12, 0, 0 );
+		_cal.set( 2011, Calendar.JULY, 26, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
