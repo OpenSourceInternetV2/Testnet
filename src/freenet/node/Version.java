afc8689638d1d387a15c6a0c8b9ea8838c4333d8
Matthew Toseland <toad@amphibian.dyndns.org>
2011-08-03 18:32:47 +0100

    Testnet build 88.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 87;
+	private static final int buildNumber = 88;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 85;
-	private static final int newLastGoodBuild = 87;
+	private static final int newLastGoodBuild = 88;
 	static final long transitionTime;
 
 	static {
