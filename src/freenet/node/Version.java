0d741b0bb66605a80128bc66a06c7482e533b2ba
Matthew Toseland <toad@amphibian.dyndns.org>
2011-08-04 02:39:35 +0100

    Testnet 93: Important bugfix in testnet 92.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 92;
+	private static final int buildNumber = 93;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 88;
-	private static final int newLastGoodBuild = 92;
+	private static final int newLastGoodBuild = 93;
 	static final long transitionTime;
 
 	static {
